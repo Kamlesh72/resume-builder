464edf76890392fe55a98b2ca486ce8ecc4ae8cc
Kamlesh Patel <kamleshpatel12328@gmail.com>
2023-08-30 09:01:13 +0530

    linkedin feature 5

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { RouterProvider, createHashRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import DashboardPage from "./Pages/DashboardPage";
 import HomePage from "./Pages/HomePage";
 
 function App() {
-  const router = createHashRouter([
+  const router = createBrowserRouter([
     { path: "/", element: <HomePage></HomePage> },
     { path: "/dashboard", element: <DashboardPage></DashboardPage> },
   ]);
