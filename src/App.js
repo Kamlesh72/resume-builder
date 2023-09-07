df4f1d786ce50ce8a0d596f63cea44c27eb8343a
Kamlesh Patel <kamleshpatel12328@gmail.com>
2023-09-07 12:06:48 +0530

    dashbaord update 4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { RouterProvider, createHashRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import DashboardPage from "./Pages/DashboardPage";
 import HomePage from "./Pages/HomePage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  const router = createHashRouter(
-    [
-      { path: "/", element: <HomePage></HomePage> },
-      { path: "/dashboard", element: <DashboardPage></DashboardPage> },
-    ],
-    { basename: "/resume-builder" }
-  );
+  const router = createBrowserRouter([
+    { path: "/", element: <HomePage></HomePage> },
+    { path: "/dashboard", element: <DashboardPage></DashboardPage> },
+    { basename: "/resume-builder" },
+  ]);
 
   return (
     <div className="App">
+      <Toaster position="top-right" />
       <RouterProvider router={router} />
     </div>
   );
